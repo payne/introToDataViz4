836b35f0c1897b70904ac34a90f57d61dac58bcf
sethvincent <sethvincent@gmail.com>
2013-04-25 19:29:34 -0700

    getJSON

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+$.getJSON('http://data.seattle.gov/api/views/3k2p-39jp/rows.json?jsonp=?&max_rows=25', function(results) {
+  
+  console.log(results.data);
+  
+  $.each(results.data, function(i, value) {
+    console.log(results['data'][i][16]);
+  });
+});
+
+
+
+
+var map = L.map('map').setView([47.6097, -122.3331], 12);
 
 L.tileLayer('http://{s}.tile.cloudmade.com/61d2e95f748e4d1da82b12d461f5174e/997/256/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
